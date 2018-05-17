123eeb2f11ced53c8392c2053c4ef2d99bb4911a
Leo Q <leoq91@gmail.com>
2018-05-17 09:53:40 -0700

    update jest

--- src/components/home/Layout.js
+++ src/components/home/Layout.js
@@ -4,70 +4,6 @@ import HydrologyCard from './HydrologyCard';
 import MorphologyCard from './MorphologyCard';
 import EcologyCard from './EcologyCard';
 import Loader from '../shared/loader/Loader';
-import UserTour from '../shared/UserTour';
-
-const homeTourSteps = [
-  {
-    step: 1,
-    selector: '.tour-logo',
-    title: <div className="tour-title">Home Button</div>,
-    body: (
-      <div className="tour-body">
-        {
-          'Click on the Eflow logo will direct you to home page anywhere in the app!'
-        }
-      </div>
-    ),
-    position: 'bottom',
-  },
-  {
-    step: 2,
-    selector: '.tour-hydrology',
-    title: <div className="tour-title">Navigation Bar Items</div>,
-    body: (
-      <div className="tour-body">
-        {
-          'Click on any item belongs to navigation bar will take you to its home page!'
-        }
-      </div>
-    ),
-  },
-  {
-    step: 3,
-    selector: '.tour-explore-hydrology',
-    title: <div className="tour-title">Explore Hydrology</div>,
-    body: (
-      <div className="tour-body">
-        This is the same as Hydrology tab on Navigation bar!
-      </div>
-    ),
-    position: 'bottom',
-  },
-  {
-    step: 4,
-    selector: '.tour-how-hydrology',
-    title: <div className="tour-title">How does it work?</div>,
-    body: (
-      <div className="tour-body">
-        This will direct you to metric summary page!
-      </div>
-    ),
-    position: 'bottom',
-  },
-  {
-    step: 5,
-    selector: '.tour-citation',
-    title: <div className="tour-title">Citation</div>,
-    body: (
-      <div className="tour-body">
-        {
-          'Click on this yellow band will copy citation information into your clipboard, so you can paste (Cmd + v or Ctrl + v) in your document!'
-        }
-      </div>
-    ),
-    position: 'top',
-  },
-];
 
 export default class Layout extends React.Component {
   constructor(props) {
@@ -88,17 +24,14 @@ export default class Layout extends React.Component {
 
   render() {
     return (
-      <React.Fragment>
-        <UserTour tourSteps={homeTourSteps} />
-        <div className="homeContainer">
-          <Loader loading={this.state.loading} />
-          <div className="animated fadeInUp">
-            <HydrologyCard />
-            <MorphologyCard />
-            <EcologyCard />
-          </div>
+      <div className="homeContainer">
+        <Loader loading={this.state.loading} />
+        <div className="animated fadeInUp">
+          <HydrologyCard />
+          <MorphologyCard />
+          <EcologyCard />
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 }
