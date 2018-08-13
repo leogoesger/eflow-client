088d0f881740ce81f5159aba5ab518543be229a2
Leo Q <leoq91@gmail.com>
2018-08-13 12:30:55 -0700

    deployment

--- src/APIs/admin.js
+++ src/APIs/admin.js
@@ -7,7 +7,9 @@ const requests = {
       .send({ ff_jwt: localStorage.getItem("ff_jwt") }),
   updateGaugeMetric: id =>
     request
-      .get(`${process.env.SERVER_ADDRESS}/api/admin/update-gauge-metrics/${id}`)
+      .post(
+        `${process.env.SERVER_ADDRESS}/api/admin/update-gauge-metrics/${id}`
+      )
       .send({ ff_jwt: localStorage.getItem("ff_jwt") }),
   broadcastMessage: msg => {
     return request
@@ -16,19 +18,19 @@ const requests = {
   },
   uploadFlowData: () =>
     request
-      .get(`${process.env.SERVER_ADDRESS}/api/admin/upload-flow-date`)
+      .post(`${process.env.SERVER_ADDRESS}/api/admin/upload-flow-date`)
       .send({ ff_jwt: localStorage.getItem("ff_jwt") }),
   uploadMetricResult: () =>
     request
-      .get(`${process.env.SERVER_ADDRESS}/api/admin/upload-metric-result`)
+      .post(`${process.env.SERVER_ADDRESS}/api/admin/upload-metric-result`)
       .send({ ff_jwt: localStorage.getItem("ff_jwt") }),
   uploadClassHydrograph: () =>
     request
-      .get(`${process.env.SERVER_ADDRESS}/api/admin/upload-class-hydrograph`)
+      .post(`${process.env.SERVER_ADDRESS}/api/admin/upload-class-hydrograph`)
       .send({ ff_jwt: localStorage.getItem("ff_jwt") }),
   uploadGaugeHydrograph: () =>
     request
-      .get(`${process.env.SERVER_ADDRESS}/api/admin/upload-gauge-hydrograph`)
+      .post(`${process.env.SERVER_ADDRESS}/api/admin/upload-gauge-hydrograph`)
       .send({ ff_jwt: localStorage.getItem("ff_jwt") }),
 };
 
