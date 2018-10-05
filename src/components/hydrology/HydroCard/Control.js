c4daf01691f95525864941ef267d8a928bfd7162
Madhav Parekh <madhavparekh@gmail.com>
2018-10-05 08:03:03 -0700

    FF-297 persistent metric toggles

--- src/components/hydrology/HydroCard/Control.js
+++ src/components/hydrology/HydroCard/Control.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Checkbox from "material-ui/Checkbox";
+//import Checkbox from "material-ui/Checkbox";
 import { find } from "lodash";
 import FlatButton from "material-ui/FlatButton";
 
@@ -8,6 +8,8 @@ import Sidebar from "./Sidebar";
 import { metricReference } from "../../../constants/metrics";
 import { Colors } from "../../../styles";
 import Setting from "material-ui/svg-icons/action/settings";
+
+var curCond = "ALL";
 
 export default class Control extends React.Component {
   constructor(props) {
@@ -42,40 +44,70 @@ export default class Control extends React.Component {
 
   componentWillUnmount() {
     //Reset all boxplot data to null
-    this.resetStates();
+    this.resetStates("ALL", true);
   }
 
-  resetStates() {
-    Object.keys(this.state).forEach(key => {
-      if (key !== "open") {
-        const currentMetric = find(metricReference, m => m.name == key);
-        this.props.overLayBoxPlotMethods[
-          currentMetric.boxPlotOverLayMethods[1]
-        ]({
-          type: currentMetric.name,
-        });
-        this.setState({ [key]: false });
-      }
-    });
-  }
-  _toggleCheckBox(metricObject, condition) {
-    this.setState({ [metricObject.name]: !this.state[metricObject.name] });
+  resetStates(condition, willMount) {
+    let condChanged = false;
 
+    if (curCond && curCond !== condition) {
+      condChanged = true;
+      curCond = condition;
+    } else curCond = condition;
+
+    if (willMount || condChanged) {
+      Object.keys(this.state).forEach(key => {
+        if (key !== "open") {
+          const currentMetric = find(metricReference, m => m.name == key);
+
+          this.props.overLayBoxPlotMethods[
+            currentMetric.boxPlotOverLayMethods[1]
+          ]({
+            type: currentMetric.name,
+          });
+          //this.setState({ [key]: false });
+
+          if (this.state[key] && condChanged) {
+            this.fetchMetricData(currentMetric, condition);
+          }
+        }
+      });
+    }
+  }
+
+  fetchMetricData(currentMetric, condition) {
     const currentType = this.props.currentGauge
       ? { type: "gaugeId", id: this.props.currentGauge.id }
       : { type: "classId", id: this.props.currentClassification.id };
 
+    return this.props.overLayBoxPlotMethods[
+      currentMetric.boxPlotOverLayMethods[0]
+    ]({
+      fetchData: {
+        [currentType.type]: currentType.id,
+        metric: currentMetric.columnName,
+        condition: condition === "ALL" ? null : condition,
+      },
+      type: currentMetric.name,
+    });
+  }
+
+  _toggleCheckBox(metricObject, condition) {
+    this.setState({ [metricObject.name]: !this.state[metricObject.name] });
+
     if (!this.state[metricObject.name]) {
-      return this.props.overLayBoxPlotMethods[
-        metricObject.boxPlotOverLayMethods[0]
-      ]({
-        fetchData: {
-          [currentType.type]: currentType.id,
-          metric: metricObject.columnName,
-          condition: condition === "ALL" ? null : condition,
-        },
-        type: metricObject.name,
-      });
+      this.fetchMetricData(metricObject, condition);
+
+      // return this.props.overLayBoxPlotMethods[
+      //   metricObject.boxPlotOverLayMethods[0]
+      // ]({
+      //   fetchData: {
+      //     [currentType.type]: currentType.id,
+      //     metric: metricObject.columnName,
+      //     condition: condition === "ALL" ? null : condition,
+      //   },
+      //   type: metricObject.name,
+      // });
     } else {
       return this.props.overLayBoxPlotMethods[
         metricObject.boxPlotOverLayMethods[1]
@@ -85,30 +117,30 @@ export default class Control extends React.Component {
     }
   }
 
-  _getDisplay(name) {
-    if (name.length > 20) {
-      return name.slice(0, 15);
-    }
-    return name;
-  }
+  // _getDisplay(name) {
+  //   if (name.length > 20) {
+  //     return name.slice(0, 15);
+  //   }
+  //   return name;
+  // }
 
-  _renderCheckBox() {
-    return metricReference.map(metricObject => {
-      if (metricObject.isBoxplotOverlay) {
-        return (
-          <Checkbox
-            key={metricObject.name}
-            checked={this.state[metricObject.name]}
-            label={this._getDisplay(metricObject.display)}
-            style={styles.checkbox}
-            labelStyle={styles.labelStyle}
-            iconStyle={{ width: "16px", fill: metricObject.colors[0] }}
-            onClick={() => this._toggleCheckBox(metricObject)}
-          />
-        );
-      }
-    });
-  }
+  // _renderCheckBox() {
+  //   return metricReference.map(metricObject => {
+  //     if (metricObject.isBoxplotOverlay) {
+  //       return (
+  //         <Checkbox
+  //           key={metricObject.name}
+  //           checked={this.state[metricObject.name]}
+  //           label={this._getDisplay(metricObject.display)}
+  //           style={styles.checkbox}
+  //           labelStyle={styles.labelStyle}
+  //           iconStyle={{ width: "16px", fill: metricObject.colors[0] }}
+  //           onClick={() => this._toggleCheckBox(metricObject)}
+  //         />
+  //       );
+  //     }
+  //   });
+  // }
 
   render() {
     return (
@@ -131,7 +163,7 @@ export default class Control extends React.Component {
           toggleDrawer={() => this.handleToggle()}
           toggleMinMax={() => this.props.toggleMinMax()}
           minMax={this.props.minMax}
-          resetStates={() => this.resetStates()}
+          resetStates={e => this.resetStates(e)}
         />
       </div>
     );
@@ -146,25 +178,25 @@ Control.propTypes = {
   minMax: PropTypes.bool.isRequired,
 };
 
-const styles = {
-  btnContainer: {
-    margin: "0px auto",
-    width: "95%",
-    position: "relative",
-  },
-  rightBtn: {
-    position: "absolute",
-    right: "0px",
-    bottom: "-60px",
-    width: "350px",
-    height: "36px",
-  },
-  checkBoxContainer: {
-    width: "60%",
-    marginLeft: "20px",
-  },
-  labelStyle: {
-    color: Colors.offBlack,
-    fontSize: "12px",
-  },
-};
+// const styles = {
+//   btnContainer: {
+//     margin: "0px auto",
+//     width: "95%",
+//     position: "relative",
+//   },
+//   rightBtn: {
+//     position: "absolute",
+//     right: "0px",
+//     bottom: "-60px",
+//     width: "350px",
+//     height: "36px",
+//   },
+//   checkBoxContainer: {
+//     width: "60%",
+//     marginLeft: "20px",
+//   },
+//   labelStyle: {
+//     color: Colors.offBlack,
+//     fontSize: "12px",
+//   },
+// };
